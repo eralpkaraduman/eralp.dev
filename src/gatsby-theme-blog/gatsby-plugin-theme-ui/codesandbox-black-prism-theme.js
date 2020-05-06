0dd430566295859b90523de324747a3e6205e539
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-05-07 00:15:07 +0300

    Updated color theme

---
+++ src/gatsby-theme-blog/gatsby-plugin-theme-ui/codesandbox-black-prism-theme.js
@@ -0,0 +1,25 @@
+const theme = {
+  color: "#999999",
+  background: "#151515",
+  comment: "#5C6370",
+  keyword: "#c678dd",
+  selector: "#c678dd",
+  changed: "#c678dd",
+  operator: "#abb2bf",
+  constant: "#d19a66",
+  number: "#d19a66",
+  builtin: "#d19a66",
+  "attr-name": "#d19a66",
+  char: "#e06c75",
+  symbol: "#e06c75",
+  variable: "#e06c75",
+  tag: "#e06c75",
+  deleted: "#e06c75",
+  string: "#98c379",
+  inserted: "#98c379",
+  punctuation: "#5C6370",
+  function: "#61afef",
+  'class-name': "#e5c07b"
+};
+
+export default theme
--- src/gatsby-theme-blog/gatsby-plugin-theme-ui/colors.js
+++ src/gatsby-theme-blog/gatsby-plugin-theme-ui/colors.js
@@ -1,18 +1,21 @@
 import merge from "deepmerge"
 import defaultThemeColors from "gatsby-theme-blog/src/gatsby-plugin-theme-ui/colors"
+import prismTheme from './codesandbox-black-prism-theme'
 
 const darkGray = `#121212`
 const orangeYellow = `#D19A66`
 const lightRed = `#E06C75`
+const lightGray = `#343434`
 
 export default merge(defaultThemeColors, {
   primary: lightRed,
   heading: lightRed,
+  prism: prismTheme,
   modes: {
     dark: {
       background: darkGray,
       primary: orangeYellow,
-      highlight: orangeYellow,
+      highlight: lightGray,
     },
   },
 })
