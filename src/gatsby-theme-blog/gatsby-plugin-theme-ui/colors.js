19302b9861931c9c94ecd4723fe443882470a98e
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-05-06 23:29:41 +0300

    Updated theme colors

--- src/gatsby-theme-blog/gatsby-plugin-theme-ui/colors.js
+++ src/gatsby-theme-blog/gatsby-plugin-theme-ui/colors.js
@@ -1,25 +1,18 @@
 import merge from "deepmerge"
 import defaultThemeColors from "gatsby-theme-blog/src/gatsby-plugin-theme-ui/colors"
 
-/*
- * Want to change your theme colors?
- * Try uncommenting the color overrides below
- * to go from default purple to a blue theme
- */
-
-// const darkBlue = `#007acc`
-// const lightBlue = `#66E0FF`
-// const blueGray = `#282c35`
+const darkGray = `#121212`
+const orangeYellow = `#D19A66`
+const lightRed = `#E06C75`
 
 export default merge(defaultThemeColors, {
-  // text: blueGray,
-  // primary: darkBlue,
-  // heading: blueGray,
-  // modes: {
-  //   dark: {
-  //     background: blueGray,
-  //     primary: lightBlue,
-  //     highlight: lightBlue,
-  //   },
-  // },
+  primary: lightRed,
+  heading: lightRed,
+  modes: {
+    dark: {
+      background: darkGray,
+      primary: orangeYellow,
+      highlight: orangeYellow,
+    },
+  },
 })
